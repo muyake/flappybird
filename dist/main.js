77b129847a83d03ffa0fdb984a3f4732b59b4df9
muyake <1245570818@qq.com>
2017-11-12 15:51:21 +0800

    去掉广告

--- dist/main.js
+++ dist/main.js
@@ -94,21 +94,63 @@ exports.config = config;
 
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
+
+if (!Object.assign) {
+  // 定义assign方法
+  Object.defineProperty(Object, 'assign', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function value(target) {
+      // assign方法的第一个参数
+      'use strict';
+      // 第一个参数为空，则抛错
+
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert first argument to object');
+      }
+
+      var to = Object(target);
+      // 遍历剩余所有参数
+      for (var i = 1; i < arguments.length; i++) {
+        var nextSource = arguments[i];
+        // 参数为空，则跳过，继续下一个
+        if (nextSource === undefined || nextSource === null) {
+          continue;
+        }
+        nextSource = Object(nextSource);
+
+        // 获取改参数的所有key值，并遍历
+        var keysArray = Object.keys(nextSource);
+        for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+          var nextKey = keysArray[nextIndex];
+          var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+          // 如果不为空且可枚举，则直接浅拷贝赋值
+          if (desc !== undefined && desc.enumerable) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+      return to;
+    }
+  });
+}
+console.log('Object.assign兼容性处理');
 var lib = {
-	$: function $(selector) {
-		return document.querySelector(selector);
-	},
-	getRandom: function getRandom(num1, num2) {
-		return num1 + Math.random() * (num2 - num1);
-	},
-	is_weixin: function is_weixin() {
-		if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
-			return true;
-		}
-		return false;
-	}
+  $: function $(selector) {
+    return document.querySelector(selector);
+  },
+  getRandom: function getRandom(num1, num2) {
+    return num1 + Math.random() * (num2 - num1);
+  },
+  is_weixin: function is_weixin() {
+    if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
+      return true;
+    }
+    return false;
+  }
 };
 exports.lib = lib;
 
@@ -259,7 +301,49 @@ Object.defineProperty(exports, "__esModule", {
 
 var _config = __webpack_require__(0);
 
+if (!Object.assign) {
+    // 定义assign方法
+    Object.defineProperty(Object, 'assign', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function value(target) {
+            // assign方法的第一个参数
+            'use strict';
+            // 第一个参数为空，则抛错
+
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert first argument to object');
+            }
+
+            var to = Object(target);
+            // 遍历剩余所有参数
+            for (var i = 1; i < arguments.length; i++) {
+                var nextSource = arguments[i];
+                // 参数为空，则跳过，继续下一个
+                if (nextSource === undefined || nextSource === null) {
+                    continue;
+                }
+                nextSource = Object(nextSource);
+
+                // 获取改参数的所有key值，并遍历
+                var keysArray = Object.keys(nextSource);
+                for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                    var nextKey = keysArray[nextIndex];
+                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                    // 如果不为空且可枚举，则直接浅拷贝赋值
+                    if (desc !== undefined && desc.enumerable) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+            return to;
+        }
+    });
+}
+
 //所有行为
+//游戏所有元素的动作对象
 var behaviorList = {
     //背景图片用的这个行为
     moveLeftToRight: function moveLeftToRight() {
@@ -345,7 +429,7 @@ var behaviorList = {
     //         this.lastAdvance = time;
     //     }
     // },
-}; //游戏所有元素的动作对象
+};
 exports.default = behaviorList;
 
 /***/ }),
