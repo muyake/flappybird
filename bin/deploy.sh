2892244c12c7ef0288866f70625d2be4e5b74365
穆亚克 <muyake@meituan.com>
2018-07-29 18:05:50 +0800

    delete run build

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,2 @@
-sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no -r ./dist $DEPLOY_USER@182.92.153.2:/root/workspace/muyake/typescript
+sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no -r ./dist $DEPLOY_USER@182.92.153.2:/root/workspace/muyake/mygames/flappybird
 echo 'deploy-origin done!'
